327825c46aeeef9d346174bdedacfe0b32023d81
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-22 17:53:44 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+  
+  attr_accessor :name, :roster
+  #attr_writer :add_student
+  
+  def initialize(name)
+    @name = name
+    #@roster = roster
+    @roster = {}
+  end 
+  
+  
+  
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+
+  end
+  
+  def grade(grade)
+   @roster[grade]
+   end  
+   
+  def sort 
+    roster.sort_by {|grades, name|}
+  end 
+    
+end 
+
